e444956cf5baef638fb7498c816749315d96bf50
Martin <darkmtbg2@gmail.com>
2022-03-01 14:11:50 +0100

    First configuration commit.

---
+++ API/bnbAPI/Static/Config.cs
@@ -0,0 +1,47 @@
+﻿using bnbAPI.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bnbAPI.Static
+{
+    public class Config
+    {
+        public static string DBHost { get; private set; }
+        public static string DBPort { get; private set; }
+        public static string DBName { get; private set; }
+        public static string DBUser { get; private set; }
+        public static string DBPassword { get; private set; }
+        public static string ClientKey { get; private set; }
+        public static string ClientSecret { get; private set; }
+
+
+        public static string GetConnectionString()
+        {
+            return "";
+        }
+
+        public static void ReadConfiguration()
+        {
+            ConfigReader reader = new ConfigReader();
+
+            Dictionary<string,string> values = reader.ReadConfigFromFile(System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Config.json"));
+
+            DBHost = values["databaseHost"];
+            DBPort = values["databasePort"];
+            DBName = values["databaseName"];
+            DBUser = values["databaseUser"];
+            DBPassword = values["databasePassword"];
+            ClientKey = values["clientKey"];
+            ClientSecret = values["clientSecret"];
+
+
+
+
+
+
+        }
+
+    }
+}
