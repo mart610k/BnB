366ff861b10316429f356aa487beabb700196e6a
Leo Jensen <lion0005@zbc.dk>
2022-03-11 08:56:22 +0100

    SQL Change

--- API/bnbAPI/Service/FacilityService.cs
+++ API/bnbAPI/Service/FacilityService.cs
@@ -16,17 +16,17 @@ namespace bnbAPI.Service
 
         }
 
-        public List<FacilityDTO> GetSearchedFacilities(string[] facilities)
+        public List<FacilityDTO> GetSearchedFacilities(string[] facility)
         {
             List<FacilityDTO> facilityDTO = new List<FacilityDTO>();
             
-            for (int i = 0; i < facilities.Length; i++)
+            for (int i = 0; i < facility.Length; i++)
             {
                 MySqlConnection conn = new MySqlConnection(Config.GetConnectionString());
 
                 MySqlCommand comm = conn.CreateCommand();
-                comm.CommandText = "Select FacilityName, FacilityID from facilities Join facility_room on facilities.FacilityID = facility_room.FK_FacilityID Where FK_FacilityID = @id";
-                comm.Parameters.AddWithValue("@id", Convert.ToInt32(facilities[i]));
+                comm.CommandText = "Select FacilityName, FacilityID from facility Join facility_room on facility.FacilityID = facility_room.FK_FacilityID Where FK_FacilityID = @id";
+                comm.Parameters.AddWithValue("@id", Convert.ToInt32(facility[i]));
 
                 conn.Open();
 
@@ -42,7 +42,7 @@ namespace bnbAPI.Service
             return facilityDTO;
         }
         /*
-         SELECT DISTINCT RoomID, RoomAddress, RoomOwner, StatusName, RoomBriefDescription, FROM room JOIN status_room ON room.RoomID = status_room.FK_RoomID JOIN status ON FK_StatusID = status.StatusID JOIN facility_room ON facility_room.FK_RoomID = room.RoomID JOIN facilities on facility_room.FK_FacilityID = facilities.FacilityID WHERE facility_room.FK_FacilityID = 3 OR facility_room.FK_FacilityID = 4; 
+         SELECT DISTINCT RoomID, RoomAddress, RoomOwner, StatusName, RoomBriefDescription, FROM room JOIN status_room ON room.RoomID = status_room.FK_RoomID JOIN status ON FK_StatusID = status.StatusID JOIN facility_room ON facility_room.FK_RoomID = room.RoomID JOIN facility on facility_room.FK_FacilityID = facility.FacilityID WHERE facility_room.FK_FacilityID = 3 OR facility_room.FK_FacilityID = 4; 
          */
         public List<FacilityDTO> GetFacilities(int id)
         {
@@ -51,7 +51,7 @@ namespace bnbAPI.Service
 
             MySqlCommand comm = conn.CreateCommand();
 
-            comm.CommandText = "SELECT FacilityID, FacilityName FROM facilities JOIN facility_room ON facilities.FacilityID = facility_room.FK_FacilityID JOIN room ON FK_RoomID = room.RoomID where RoomID = @id;";
+            comm.CommandText = "SELECT FacilityID, FacilityName FROM facility JOIN facility_room ON facility.FacilityID = facility_room.FK_FacilityID JOIN room ON FK_RoomID = room.RoomID where RoomID = @id;";
             comm.Parameters.AddWithValue("@id", id);
 
             conn.Open();
@@ -74,7 +74,7 @@ namespace bnbAPI.Service
 
             MySqlCommand comm = conn.CreateCommand();
 
-            comm.CommandText = "SELECT * FROM facilities";
+            comm.CommandText = "SELECT * FROM facility";
 
             conn.Open();
 
