dda4e53decc7f8914669723eeb7e7c6e91eefc9c
mart610k <53937525+mart610k@users.noreply.github.com>
2022-03-11 08:54:04 +0100

    Update 4-CreateDetailedRoomTables.sql

--- Database/4-CreateDetailedRoomTables.sql
+++ Database/4-CreateDetailedRoomTables.sql
@@ -4,8 +4,7 @@ CREATE TABLE `picture` (
   `FileName` binary(16) NOT NULL,
   `fk_roomid` int NOT NULL,
   PRIMARY KEY (`FileName`),
-  KEY `fk_picture_room_id` (`fk_roomid`),
-  CONSTRAINT `fk_picture_room` FOREIGN KEY (`fk_roomid`) REFERENCES `room` (`roomid`)
+  FOREIGN KEY (`fk_roomid`) REFERENCES `room` (`roomid`)
 );
 
 CREATE TABLE `facility` (
@@ -18,6 +17,6 @@ CREATE TABLE `facility_room` (
   `fk_roomid` int NOT NULL,
   `fk_facilityid` int NOT NULL,
   PRIMARY KEY (`fk_roomid`,`fk_facilityid`),
-  FOREIGN KEY (`fk_roomID`) REFERENCES `room` (`roomid`),
+  FOREIGN KEY (`fk_roomid`) REFERENCES `room` (`roomid`),
   FOREIGN KEY (`fk_facilityid`) REFERENCES `facility` (`facilityid`)
 );
